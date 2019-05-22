5122af52ee85edb3d2d2ff4865b5d7824d5ae242
mayo <shiroshi.hiro@gmail.com>
2019-05-22 03:12:14 +0000

    add column and validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
         @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         
